1575b063503a9c08fd4e17f431e9cc6010036eab
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-17 13:00:16 +0100

    FUNCIONA!

--- Ejercicios/script.js
+++ Ejercicios/script.js
@@ -36,19 +36,35 @@ function mostrarCookies(elEvento){
             punto.innerHTML = arrayCookies[i];
             lista.appendChild(punto);
         }
-
 }     
 
 function modificarCookies(elEvento){
-    var nombreCookie = prompt("Nombre de la cookie:");
-    var valorCookie = prompt("Valor de la cookie:");
+    var nombreCookie = prompt("Nombre de la cookie a modificar:");
+    var valorCookie = prompt("Nuevo valor de la cookie:");
     var fechaValor = prompt("¿Cuánto tiempo quieres que dure tu cookie?")
     document.cookie= nombreCookie + "=" + valorCookie + "; + max-age=" + fechaValor;
 }
 
 function eliminarCookies(elEvento){
     var nombreCookie = prompt("¿Qué cookie desea eliminar?");
-    document.cookie = nombreCookie +"=" + null + ";expires = 1 Jan 1970 23:59:59 GMT";
+    //document.cookie = nombreCookie +"=" + null + ";expires = 1 Jan 1970 23:59:59 GMT";
+
+    var micookie = nombreCookie + "=";
+    var arrayCookies = document.cookie.split(';');
+
+    for(var i=0; i<arrayCookies.length; i++) {
+        var c = arrayCookies[i];
+        while (c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+    }
+
+    if (c.indexOf(micookie) == 0){
+        document.cookie = nombreCookie +"=" + null + ";expires = 1 Jan 1970 23:59:59 GMT";
+        alert("La cookie seleccionada ha sido eliminada");
+    } else {
+        alert("La cookie seleccionada no existe");
+    }
 }
 
 window.onload=function(){
