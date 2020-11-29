68e9c3764a680d6f6c861f3ff075ce500d78f730
InmaaSL <73943042+InmaaSL@users.noreply.github.com>
2020-11-29 21:04:46 +0100

    Ejercicio 9.02 OK!

--- Tema 2/Ejercicio 9. JSON/02/script.js
+++ Tema 2/Ejercicio 9. JSON/02/script.js
@@ -1,58 +1,85 @@
 function inicio(){
+
+    //Recogemos los botones importantes para poder usarlos posteriormente:
     var botonGuardar = document.getElementById("guardar");
     var botonRecuperar = document.getElementById("recuperar");
     var botonBorrar = document.getElementById("borrar");
 
-    var nombre = document.getElementById("nombre");
-    var apellidos = document.getElementById("apellidos");
-    var dni = document.getElementById("dni");
+    //Obtenemos la varibale que recoge los checkbox de los idiomas:
+    idiomas = document.getElementsByName("checkbox");
 
-    var mostrar = document.getElementById("mostrar");
-
+    //Le damos funcionamiento a los botones:
     botonGuardar.onclick = guardar;
     botonRecuperar.onclick = recuperar;
     botonBorrar.onclick = borrar;
 }
 
 function guardar(elEvento){
-    var idioma = document.getElementsByName("checkbox");
-    var seleccionados="";
+    //Recogemos las variables necesarias:
+    var nombre = document.getElementById("nombre");
+    var apellidos = document.getElementById("apellidos");
+    var dni = document.getElementById("dni");
+    var email = document.getElementById('email');
 
-    let checked = document.querySelectorAll('input[type=checkbox]:checked');
-    
-    if (checked.length == 0) {
-        //SINO HA SELECCIONADO
-        alert("¡ERROR! No ha seleccinado ningún idioma");
-    } else {
-        for (var x = 0; x < idioma.length; x++){
-            if(idioma[x].checked){
-                console.log(idioma[x].checked);
-                seleccionados = seleccionados + idioma[x].value + " ";
+    //Creamos un array con los objetos que vamos a usar:
+    miObj = { nombre, apellidos, dni, email, idiomas};
+        miObj.nombre = nombre.value;
+        miObj.apellidos = apellidos.value;
+        miObj.dni = dni.value;
+        miObj.email = email.value; 
+        miObj.idiomas = new Array();
+
+        idiomas.forEach(idioma => {
+            if(idioma.checked){
+                miObj.idiomas.push(idioma.value);
             }
-        }
-    }
+        });
 
-    localStorage.setItem("Nombre", nombre.value);
-    localStorage.setItem("Apellidos", apellidos.value);
-    localStorage.setItem("DNI", dni.value);
-    localStorage.setItem("Idioma", seleccionados);
+        //Lo guardamos en el localStorage como JSON con la función stringifi:
+        localStorage.setItem('formulario', JSON.stringify(miObj));
+    
+    //Volvemos a la ventana local:
+    window.location.href = "formulario.html";
 
 }
 
 function recuperar(elEvento){
-    mostrar.innerHTML = "";
-    for( let i = 0; i < localStorage.length; i++){
-        mostrar.innerHTML = mostrar.innerHTML + localStorage.key(i) + " - " + localStorage[localStorage.key(i)] + "</br>";
-    }
+    //Comprobamos previamente que haya algo almacenado en el localstorage:
+    if(localStorage.getItem('formulario')){
+
+        //Recogemos en un objeto la información que haya en el localStorage pasandolo a string con parse:
+        miObj = JSON.parse(localStorage.getItem('formulario')); 
+
+        //Colocamos cada valor en su caja:
+        nombre.value = miObj.nombre;
+		apellidos.value = miObj.apellidos;
+		dni.value = miObj.dni;
+		email.value = miObj.email;
+
+        //Seleccionamos los idiomas que estén guardados: 
+		idiomas.forEach(idiomas => {
+			miObj.idiomas.forEach(value => {
+				if (idiomas.value == value) {
+					idiomas.checked = true;
+				}
+			});
+		});
+	}
+	else {
+		alert('No se encuentran datos en el registro');
+	}     
 }
 
 function borrar(elEvento){
-    localStorage.clear();
-    alert("¡LocalStorage borrado!")
-    recuperar();
-    window.location.href = "formulario.html";
+    //Comprobamos previamente que haya algo almacenado en el localstorage:
+    if(localStorage.getItem('formulario')){
+        localStorage.clear();
+        alert("¡LocalStorage borrado!")
+        window.location.href = "formulario.html";
+    } else {
+        alert('No se encuentran datos en el registro');
+    }
 }
-
 
 window.onload=function(){
     inicio();
